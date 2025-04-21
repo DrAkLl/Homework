70a045ce725778014ada4f91875420a738de4926
DrAkLl <bikoffartyom@yandex.ru>
2025-04-21 11:23:04 +0700

    Create 1.cs

---
+++ C#/1.cs
@@ -0,0 +1,8 @@
+Console.Write("Как тебя зовут?");
+string? name = Console.ReadLine();
+Console.WriteLine($"Привет {name}!");
+
+string qu = "Как дела?";
+Console.Write($"{qu}");
+Console.Write($"{qu}");
+Console.Write($"{qu}");
